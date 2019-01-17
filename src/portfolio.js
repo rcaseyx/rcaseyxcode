03820f092c372242a5ddc158217ff3f910ed1fc8
Ryan Casey <rcaseyx@gmail.com>
2019-01-17 09:14:01 -0500

    updated descriptions

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,21 +10,21 @@ export default class Portfolio extends React.Component {
             projects: [
                 {
                   title: "New Best Friend",
-                  about: "New Best Friend allows users to view available rescue dogs for adoption. Users can set preferences and add dogs to their profile.",
+                  about: "New Best Friend allows users to view available rescue dogs for adoption. Users can set preferences and add dogs to their profile. Built with React and Node.",
                   image: "https://github.com/rcaseyx/new-best-friend/raw/master/public/images/findfriend.png",
                   url: "https://github.com/rcaseyx/new-best-friend",
                   liveUrl: "https://new-best-friend.herokuapp.com/"
                 },
                 {
                   title: "Seen It?",
-                about: "Seen It is an interactive application allowing users to keep track of the movies they watch. Users are able to maintain lists of movies they've seen, as well as view other user lists.",
+              about: "Seen It is an interactive application allowing users to keep track of the movies they watch. Users are able to maintain lists of movies they've seen, as well as view other user lists. Built with jQuery and Node.",
                   image: "https://github.com/rcaseyx/seen-it/raw/master/images/seen-it-4.png",
                   url: "https://github.com/rcaseyx/seen-it",
                   liveUrl: "https://seen-it-app.herokuapp.com/"
                 },
                 {
                   title: "The Scoop",
-                  about: "The Scoop is an app that allows users to search for movies and shows and get information about them. Users can see recommended titles from a 3rd party API, as well as the trailer via the YouTube Data API.",
+                  about: "The Scoop is an app that allows users to search for movies and shows and get information about them. Built with jQuery and integrated with the YouTube Data API, The Movie Db API, and the Best Buy API.",
                   image: "https://github.com/rcaseyx/the-scoop/raw/master/imgs/scoop2.png",
                   url: "https://github.com/rcaseyx/the-scoop",
                   liveUrl: "https://rcaseyx.github.io/the-scoop"
