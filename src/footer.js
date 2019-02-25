54cd7c4212c8f18f381db51511a184ab6d4c9689
Ryan Casey <rcaseyx@gmail.com>
2019-02-25 11:27:23 -0500

    added link to brigs site

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,8 @@ import './footer.css';
 export default function Footer(props) {
     return (
         <footer role="contentinfo">
-            This site was built by me using React. <a href="https://github.com/rcaseyx/rcaseyxcode" target="_blank" rel="noopener noreferrer">View the code</a>. Logo and color scheme design by Brigette Margeanu Hall.
+            This site was built by me using React. <a href="https://github.com/rcaseyx/rcaseyxcode" target="_blank" rel="noopener noreferrer">View the code</a>.
+            Logo and color scheme design by <a href="http://www.brigettemargeanu.com/" target="_blank" rel="noopener noreferrer">Brigette Margeanu Hall</a>.
         </footer>
     );
 }
